9310201e2e303fb1a8a11b278b4d6b253c29848c
sudhir <sudhirbitsgoa@gmail.com>
2019-02-25 20:16:35 +0530

    adding blocked to get contacts method

--- packages/rocketchat-lib/server/methods/getContacts.js
+++ packages/rocketchat-lib/server/methods/getContacts.js
@@ -6,14 +6,20 @@ Meteor.methods({
 
 		const contactsFinal = RocketChat.models.Contacts.findByUserId(Meteor.userId());
 		let contacts = [];
-		console.log('the contacts final', contactsFinal);
 		if (!contactsFinal) { // no contacts for the user
 			return [];
 		}
 		contacts = contacts.concat(contactsFinal.contacts);
-
+		const blockContactsHash = {};
+		const blockContacts = contactsFinal.block_contacts;
+		blockContacts.forEach(bc => {
+			blockContactsHash[bc] = 1;
+		});
 		const users = RocketChat.models.Users.findByIds(contacts).fetch();
 		for (let i = users.length - 1; i >= 0; i--) {
+			if (blockContactsHash[users[i]._id]) {
+				users[i].blocked = true;
+			}
 			delete users[i].services;
 			delete users[i].settings;
 		}
